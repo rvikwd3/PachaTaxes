3afd72c4a8669c2e6b3c45fae961416a030122b5
rvikwd3 <rvikwd3@outlook.com>
2018-07-02 16:57:34 +0530

    Added getDate

--- docread.py
+++ docread.py
@@ -14,43 +14,46 @@ def main():
     # Number of tables
     print 'Tables:\t{}'.format(document.tables.__len__())
 
-    # Iterate over each table
+   # Iterate over each table
     for table_index in range(document.tables.__len__()):
 
         # GSTNumber Cell Index
         print "\nTable {}\tGST Index:\t{}\n".format(table_index, getGSTIndex(document.tables[table_index]))
         # Table text
-        print "\nTable {} Text:\n{}".format(table_index, tableText(document.tables[table_index]))
+        print "\nTable {} Text:\n{}".format(table_index, getTableText(document.tables[table_index]))
+
+    # Date
+    print "\n Table {} Date:\n{}".format('0', getDate(document.tables[0]))
 
 
 # =====================================
 # FUNCTIONS
 # =====================================
 
-#   1.  tableText()
+#   1.  getTableText()
 #   2.  getGSTIndex()
 
 
 # =====================================
-# tableText()
+# getTableText()
 # Text within given table
 
-def tableText(table):
+def getTableText(table):
     table_text = ""
     old_text = ""
 
-#   Number of rows & Columns
+    #   Number of rows & Columns
     print 'Rows:\t{}'.format(table.rows.__len__())
     table_rows = table.rows.__len__()
     print 'Cols:\t{}\n'.format(table.columns.__len__())
     table_cols = table.columns.__len__()
 
-#   Text within table
+    #   Text within table
     for i in range(table_rows):
             for j in range(table_cols):
 
             #   (i,j) goes out of cell range for some reason
-            #   -> we handle the "out of index range" error
+            #   we handle the "out of index range" error
                 try:
                     new_text = table.cell(i,j).text
                 except IndexError:
@@ -70,14 +73,15 @@ def tableText(table):
 # =====================================
 # getGSTIndex()
 # Index of cell containing GSTNumber
+# Only checks first cell(0,0) of table
 
 def getGSTIndex(table):
 
-#   Check if GST number is in first cell
+    #   Check if GST number is in first cell
     gst_cell = table.cell(0,0).text
     gst_index = next(((index, i) for (index,i) in enumerate(gst_cell.split()) if re.search(r'GSTIN', gst_cell.split()[index], flags=re.M|re.S) is not None), (-1,-1))[0]
 
-#   If GST number wasn't found then gst_index <- -1
+    #   If GST number wasn't found then gst_index <- -1
     if gst_index != -1:
             print '\nGST Index:\t{}'.format(gst_index)
             print '\nGST Number:\t{}'.format(gst_cell.split()[gst_index + 1])
@@ -86,6 +90,22 @@ def getGSTIndex(table):
 
     return gst_index
 
+# =====================================
+# getDate()
+# Date of invoice
+
+def getDate(table):
+    table_text = getTableText(table)
+
+    #Regex search for Dated:
+    date_exists = re.search(r'Dated:\d{2}-[a-zA-z]+-\d{4}', table_text, flags=re.M|re.S)
+
+    if date_exists:
+        date = date_exists.group(0)[6:]
+    else:
+        date = 'N/A'
+
+    return date
 
 # =====================================
 # Runtime 
